38669ebf19e0bdedc4c9535c4ed601d2ee927d5b
chrisegan623 <54693774+chrisegan623@users.noreply.github.com>
2019-11-14 15:05:04 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    iris = load_iris()
+    data = pd.DataFrame(iris.data)  # convert to pandas dataframe
+    data.columns = iris.feature_names  # set column names to feature names
+
+    distance = []
+
+    for k in range(1,10):
+        model = KMeans(n_clusters=k).fit(data)
+        model.fit(data)
+        distance.append(sum(np.min(cdist(data,model.cluster_centers_,'euclidean'), axis=1)) / data.shape[0])
+
+    plt.plot(range(1,10), distance, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distance')
+    plt.title('The Elbow Method')
+    plt.savefig('Clusters.pdf')
+    plt.show()
